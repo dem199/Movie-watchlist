8680426f6bffe79cc63ef9bc25fd15eafc4304b0
Opeyemi Olatunbosun <olatunbosunopeyemi186035@gmail.com>
2023-06-13 17:14:53 +0100

    index.js
    added error message

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-# Movie Searchapp
+# Vanilla Movie Search App
 
-This Movie Search App is a user-friendly web application developed using vanilla JavaScript. It offers a seamless and efficient way for users to search for their favorite movies and discover new ones. With a clean and intuitive interface, users can enter keywords, movie titles, or actor names to retrieve relevant movie results. The app leverages an API to fetch real-time data and provides users with detailed information about each movie, including the title, release date, synopsis, cast, and rating
+The Movie Search App is a user-friendly application that allows users to search for movies based on their titles. With its intuitive interface and seamless integration with a movie database API, users can easily find detailed information about their favorite films. The app displays movie titles, photos, release dates, overviews, and star ratings, providing users with a comprehensive view of each movie. Additionally, users can add movies to their watchlist with a simple click, and the selected movies are saved in local storage for future reference. The watchlist page allows users to view their saved movies and provides a remove button next to each movie, enabling easy removal from the watchlist.
+
+## Key Features
+
+- ### Movie Search:
+
+Users can enter the title of a movie in the input element to search for specific films.
+
+- ### Movie Details:
+
+The app fetches movie information from an API and displays essential details such as the movie title, photo, release date, overview, and star ratings.
+
+- ### Watchlist:
+
+Users can add movies to their watchlist by clicking the "Add" button. The selected movies are saved in local storage for easy access later.
+
+- ### Watchlist Management:
+
+The watchlist page displays the movies saved by the user, along with a remove button next to each movie. Users can remove movies from the watchlist with a simple click.
+
+## Getting Started
+
+To get started with the Movie Search App, follow these steps:
+
+1. #### Clone the repository:
+
+```bash
+
+git https://github.com/dem199/Movie-watchlist
+
+```
+
+2. #### Install the required dependencies:
+
+```bash
+
+cd movie watchlist
+npm install
+
+```
+
+3. #### Launch the app locally:
+
+```sql
+
+npm start
+
+```
+
+# Contributing
+
+Contributions to the Movie Search App are welcome! If you have any suggestions, bug fixes, or additional features you'd like to see, feel free to open an issue or submit a pull request.
+
+Enjoy using the Movie Search App and discovering information about your favorite movies with ease. Happy searching and movie watching!
--- index.js
+++ index.js
@@ -1,112 +1,105 @@
 function findMovie() {
-    const movieTitle = document.getElementById('input-text').value
+  const movieTitle = document.getElementById('input-text').value;
 
-    if (movieTitle) {
-        document.getElementsByTagName('main')[0].innerHTML = `
-        <div id="search-box">
-
-        </div>
-        `
-        fetch(`https://www.omdbapi.com/?s=${movieTitle}&apikey=ce8ec4bc`)
-        .then(res => res.json())
-            .then(data => {
-                data.Search.map(movie => {
-                    fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=ce8ec4bc`)
-                      .then(res => res.json())
-                      .then(data => {
-                        fillHtml(data, ["saveData(this)","Watchlist", "Icon (1)"])
-                      })
-          
-            })
-        })
-        .catch((error) => {
-            console.error('Errores:', error);
-            clearMain()
+  if (movieTitle) {
+    document.getElementsByTagName('main')[0].innerHTML = `
+      <div id="search-box"></div>
+    `;
+    fetch(`https://www.omdbapi.com/?s=${movieTitle}&apikey=ce8ec4bc`)
+      .then(res => res.json())
+      .then(data => {
+        let searchResults = data.Search;
+        if (searchResults && searchResults.length > 0) {
+          searchResults.map(movie => {
+            fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=ce8ec4bc`)
+              .then(res => res.json())
+              .then(data => {
+                fillHtml(data, ["saveData(this)", "Watchlist", "Icon (1)"]);
+              });
           });
-      } else {
-        clearMain()
-        
-    }
+        } else {
+          const searchBox = document.getElementById('search-box');
+          searchBox.innerHTML = '<p class="error-message">No movies found.</p>';
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        clearMain();
+      });
+  } else {
+    clearMain();
+  }
 }
 
-
-
 function loadStorageContent() {
-    event.preventDefault()
-    document.querySelector('.search-container').style.display = 'none'
-    document.querySelector('.header-info-sub').innerHTML = `
+  event.preventDefault();
+  document.querySelector('.search-container').style.display = 'none';
+  document.querySelector('.header-info-sub').innerHTML = `
     <a href="#" onclick="location.reload()">Find your film</a>
-    `
-    document.querySelector('.header-info-main').textContent = 'My watchlist'
-    if (localStorage.length) {
-      document.getElementsByTagName('main')[0].innerHTML = `
+  `;
+  document.querySelector('.header-info-main').textContent = 'My watchlist';
+  if (localStorage.length) {
+    document.getElementsByTagName('main')[0].innerHTML = `
       <div id="search-box"></div>
-      `
-      let imdbS = Object.keys(localStorage)
-      for (let imdb of imdbS) {
-        fetch(`https://www.omdbapi.com/?i=${imdb}&apikey=ce8ec4bc`)
-          .then(res => res.json())
-          .then(data => {
-            fillHtml(data, ["removeData(this)","Remove","Icon (3)"])
-          })
-      }
-    } else {
-      clearMain()
+    `;
+    let imdbS = Object.keys(localStorage);
+    for (let imdb of imdbS) {
+      fetch(`https://www.omdbapi.com/?i=${imdb}&apikey=ce8ec4bc`)
+        .then(res => res.json())
+        .then(data => {
+          fillHtml(data, ["removeData(this)", "Remove", "Icon (3)"]);
+        });
     }
+  } else {
+    clearMain();
+  }
 }
 
 function saveData(local) {
-    event.preventDefault()
-    localStorage.setItem(local.name, local.name)
-    // console.log(local.textContent)
-    local.textContent = "✔Added"
+  event.preventDefault();
+  localStorage.setItem(local.name, local.name);
+  local.textContent = "✔Added";
 }
-  
-  
-  function removeData(localKey) {
-    event.preventDefault()
-    localStorage.removeItem(localKey.name)
-      localKey.parentElement.parentElement.parentElement.remove()
+
+function removeData(localKey) {
+  event.preventDefault();
+  localStorage.removeItem(localKey.name);
+  localKey.parentElement.parentElement.parentElement.remove();
 }
-  
-  
-  function clearMain() {
-    document.getElementsByTagName('main')[0].innerHTML = `
-     
-      <div class="blank-details" >
-          <img src="image/Icon (2).png" alt="" />
-          <h1>Start exploring</h1>
-      </div>
-      <div id="search-box"></div>
-      `
+
+function clearMain() {
+  document.getElementsByTagName('main')[0].innerHTML = `
+    <div class="blank-details">
+      <img src="image/Icon (2).png" alt="" />
+      <h1>Start exploring</h1>
+    </div>
+    <div id="search-box"></div>
+  `;
 }
- 
+
 function fillHtml(data, action) {
   document.getElementById('search-box').innerHTML += `
-  <div class="movie-container">
-          <img src=${data.Poster} alt="" class="movie-image"/>
-          <div class="movie-info">
-            <div class="movie-info-one">
-              <h3 class="movie-title">${data.Title}</h3>
-              <img src="image/Icon.png" class="movie-rating" /><span>${data.imdbRating}</span>
-            </div>
-            <div class="movie-info-two">
-              <p class="movie-time">${data.Runtime}</p>
-              <p class="movie-genre">${data.Genre}</p>
-              <a class="watchlist-anchor" name=${data.imdbID} href="#" onclick=${action[0]}><img src="./image/${action[2]}.png"  alt="Some Data" /> ${action[1]}</a>
-            
-            </div>
-            <div class="movie-description">
-              <p class="description">
+    <div class="movie-container">
+      <img src=${data.Poster} alt="" class="movie-image"/>
+      <div class="movie-info">
+        <div class="movie-info-one">
+          <h3 class="movie-title">${data.Title}</h3>
+          <img src="image/Icon.png" class="movie-rating" /><span>${data.imdbRating}</span>
+        </div>
+        <div class="movie-info-two">
+          <p class="movie-time">${data.Runtime}</p>
+          <p class="movie-genre">${data.Genre}</p>
+          <a class="watchlist-anchor" name=${data.imdbID} href="#" onclick=${action[0]}><img src="./image/${action[2]}.png"  alt="Some Data" /> ${action[1]}</a>
+          </div>
+          <div class="movie-description">
+            <p class="description">
               ${data.Plot}
-              </p>
-            </div>
+            </p>
           </div>
         </div>
-      `
-}
-
-
+      </div>
+    `;
+  }
   
-
-
+  
+  
